35afe0278bcb85bd434718abe271ced931818a9e
Ricky Laney <rlaneyjr@gmail.com>
2016-02-19 12:12:44 -0500

    Moved bin

--- brewUpdate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Homebrew and Homebrew Cask Maintenance.  Updating, upgrading, and cleaning all brews."
-brew update && brew upgrade && brew cleanup && brew cask cleanup
-
-#echo "Looking for Homebrew problems using the Doctor and verbose maintenance."
-#brew cask doctor --verbose
-
-#brew update --verbose && brew upgrade brew-cask --verbose && brew cleanup --verbose && brew cask cleanup --verbose

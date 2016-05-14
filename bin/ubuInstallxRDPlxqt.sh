ffc58b3e2b2957042b57363f90224a64a0031993
Ricky Laney <rlaneyjr@gmail.com>
2016-05-14 17:51:53 -0400

    Fixed lxqt

--- bin/ubuInstallxRDPlxqt.sh
+++ bin/ubuInstallxRDPlxqt.sh
@@ -3,7 +3,7 @@
 # Get sudo password at begining
 sudo -v
 # Install xRDP
-sudo apt-get install xrdp
+sudo apt-get install xrdp software-properties-common
 # Add the lxqt repository
 sudo add-apt-repository ppa:lubuntu-dev/lubuntu-daily
 # Update pulling down repo stuff
@@ -11,7 +11,7 @@ sudo apt-get update
 # Upgrade
 sudo apt-get upgrade
 # Install lxqt packages
-sudo apt-get install lxqt-metapackage lxqt-session lxsession
+sudo apt-get install lxqt-metapackage openbox lxqt-panel lxqt-session lxsession
 # Add command to start lxqt at session
 echo startlxqt >~/.xsession
 # Restart xRDP

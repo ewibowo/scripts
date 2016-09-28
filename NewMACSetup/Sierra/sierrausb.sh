a9b8bdd0dabaaf20c2ed97e94e531704ff9c5932
Ricky Laney <rlaneyjr@gmail.com>
2016-09-27 22:09:17 -0400

    Auto Push

---
+++ NewMACSetup/Sierra/sierrausb.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo /Applications/Install\ macOS\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/macOSSierraInstall --applicationpath /Applications/Install\ macOS\ Sierra.app --nointeraction

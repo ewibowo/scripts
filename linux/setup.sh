ecee43064fbb26d357ea9b59755f0b86c7b96cb5
Ricky Laney <rlaneyjr@gmail.com>
2017-01-07 18:19:23 -0500

    Auto Push

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+sudo -v
+
 # Setup user
-#echo "rlaney ALL=NOPASSWD: ALL" > /etc/sudoers.d/boostrap
-#chmod 0400 /etc/sudoers.d/boostrap
+sudo echo "rlaney ALL=NOPASSWD: ALL" > /etc/sudoers.d/boostrap
+sudo chmod 0400 /etc/sudoers.d/boostrap
 #sudo groupadd admin
 #sudo usermod -g admin rlaney
 #sudo service sudo restart
@@ -12,11 +14,10 @@ general_package="git git-core zsh tmux wget curl vim python-distribute python-se
 
 #develop_package = "vnstat vde2 sqlite3 mongodb tkmib tcpick tcpreplay tcpspy tcpstat tcptrack ansible  tcpxtract tgn speedometer snort smokeping ipcalc scli redis-server qemu qemu-utils python3.4 python3.4-venv python3-pysnmp4 python3-networkx python3-ipaddr python3-ipy python3-lxc python3-netaddr python-scapy python-twisted python-pysnmp4 python-redis python-pynetsnmp python-netsnmp python-networkx python-nemu python-netaddr python-ipaddr python-ipy python-ipcalc"
 #
-sudo -v
-sudo apt-get update
-for package in $general_package; do
-sudo apt-get -y install $package
-done
+#sudo apt-get update
+#for package in $general_package; do
+#    sudo apt-get -y install $package
+#done
 #sudo apt-get -y install $develop_package
 #
 #sudo pip install virtualenv
@@ -25,19 +26,16 @@ done
 #gem install bundler
 #gem install tmuxinator
 
-git clone https://github.com/rlaneyjr/dotfiles.git ~/.dotfiles
-#git clone https://github.com/rlaneyjr/scripts.git ~/scripts
-git clone https://github.com/rlaneyjr/vim.git ~/.vim
-git clone https://github.com/rlaneyjr/zsh.git ~/.zsh
-git clone https://github.com/rlaneyjr/lib.git ~/lib
-git clone https://github.com/rlaneyjr/zplug.git ~/.zplug
-git clone https://github.com/rlaneyjr/config.git ~/.config
+#git clone https://github.com/rlaneyjr/dotfiles.git ~/.dotfiles
+##git clone https://github.com/rlaneyjr/scripts.git ~/scripts
+#git clone https://github.com/rlaneyjr/vim.git ~/.vim
+#git clone https://github.com/rlaneyjr/zsh.git ~/.zsh
+#git clone https://github.com/rlaneyjr/lib.git ~/lib
+#git clone https://github.com/rlaneyjr/zplug.git ~/.zplug
+#git clone https://github.com/rlaneyjr/config.git ~/.config
 
-mkdir -p ~/.vim/backups
-mkdir -p ~/.vim/swaps
-mkdir -p ~/.vim/undo
-mkdir -p ~/Projects
-mkdir -p ~/.virtualenvs
+#mkdir -p ~/Projects
+#mkdir -p ~/.virtualenvs
 
-source ~/scripts/bin/linkBatch.sh
-source ~/scripts/bin/linkdots.sh
+source ../bin/linkBatch.sh
+source ../bin/linkdots.sh

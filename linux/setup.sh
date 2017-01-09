79404aa7d870e2c19f3cbfd1d3e22a99ef470d73
Ricky Laney <rlaneyjr@gmail.com>
2017-01-09 09:38:38 -0500

    The git-update script pushed these

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+set e
 sudo -v
 
 # Setup user
-sudo echo "rlaney ALL=NOPASSWD: ALL" > /etc/sudoers.d/boostrap
-sudo chmod 0400 /etc/sudoers.d/boostrap
+#sudo echo "rlaney ALL=NOPASSWD: ALL" > /etc/sudoers.d/boostrap
+#sudo chmod 0400 /etc/sudoers.d/boostrap
 #sudo groupadd admin
 #sudo usermod -g admin rlaney
 #sudo service sudo restart
@@ -13,29 +14,31 @@ sudo chmod 0400 /etc/sudoers.d/boostrap
 general_package="git git-core zsh tmux wget curl vim python-distribute python-setuptools python-pip python3-pip fontconfig ruby expect virtualenv virtualenvwrapper vim-nox-py2 vim-python-jedi vim-syntastic vim-khuno python3.5-dbg python3.5-dev python3.5-doc python3.5-examples python-dbg python-dev python-doc python-examples"
 
 #develop_package = "vnstat vde2 sqlite3 mongodb tkmib tcpick tcpreplay tcpspy tcpstat tcptrack ansible  tcpxtract tgn speedometer snort smokeping ipcalc scli redis-server qemu qemu-utils python3.4 python3.4-venv python3-pysnmp4 python3-networkx python3-ipaddr python3-ipy python3-lxc python3-netaddr python-scapy python-twisted python-pysnmp4 python-redis python-pynetsnmp python-netsnmp python-networkx python-nemu python-netaddr python-ipaddr python-ipy python-ipcalc"
-#
-#sudo apt-get update
-#for package in $general_package; do
-#    sudo apt-get -y install $package
-#done
-#sudo apt-get -y install $develop_package
-#
-#sudo pip install virtualenv
-#sudo pip install virtualenvwrapper
-#
-#gem install bundler
-#gem install tmuxinator
 
-#git clone https://github.com/rlaneyjr/dotfiles.git ~/.dotfiles
-##git clone https://github.com/rlaneyjr/scripts.git ~/scripts
-#git clone https://github.com/rlaneyjr/vim.git ~/.vim
-#git clone https://github.com/rlaneyjr/zsh.git ~/.zsh
-#git clone https://github.com/rlaneyjr/lib.git ~/lib
-#git clone https://github.com/rlaneyjr/zplug.git ~/.zplug
-#git clone https://github.com/rlaneyjr/config.git ~/.config
+sudo apt-get update
+for package in $general_package; do
+    sudo apt-get -y install $package
+done
+sudo apt-get -y install $develop_package
 
-#mkdir -p ~/Projects
-#mkdir -p ~/.virtualenvs
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
 
-source ../bin/linkBatch.sh
-source ../bin/linkdots.sh
+gem install bundler
+gem install tmuxinator
+
+git clone https://github.com/rlaneyjr/dotfiles.git ~/.dotfiles
+#git clone https://github.com/rlaneyjr/scripts.git ~/scripts
+git clone https://github.com/rlaneyjr/vim.git ~/.vim
+git clone https://github.com/rlaneyjr/zsh.git ~/.zsh
+git clone https://github.com/rlaneyjr/lib.git ~/lib
+git clone https://github.com/rlaneyjr/zplug.git ~/.zplug
+git clone https://github.com/rlaneyjr/config.git ~/.config
+git clone https://github.com/rlaneyjr/tmux.git ~/.tmux
+git clone https://github.com/rlaneyjr/tmuxinator.git ~/.tmuxinator
+
+mkdir -p ~/Projects
+mkdir -p ~/.virtualenvs
+
+source $HOME/scripts/bin/linkBatch.sh
+source $HOME/scripts/bin/linkdots.sh

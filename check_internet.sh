8a011b811987eaa6399c6496f03679268ad66a7e
Ricky Laney <rlaneyjr@gmail.com>
2017-04-14 13:08:16 -0400

    The git-update script pushed these

---
+++ check_internet.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Check the internet connection with manual count set.
+((count = 100))                            # Maximum number to try.
+while [[ $count -ne 0 ]] ; do
+    ping -c 1 8.8.8.8                      # Try once.
+    rc=$?
+    if [[ $rc -eq 0 ]] ; then
+        ((count = 1))                      # If okay, flag to exit loop.
+    fi
+    ((count = count - 1))                  # So we don't go forever.
+done
+
+if [[ $rc -eq 0 ]] ; then                  # Make final determination.
+    echo `say The internet is back up.`
+else
+    echo `say Timeout.`
+fi

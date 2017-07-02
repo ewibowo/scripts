0969b9b1db8213ec9930ee758bfadc7490cfea13
Ricky Laney <rlaneyjr@gmail.com>
2017-07-01 22:08:58 -0400

    The git-update script pushed these

--- bin/docker-completion.sh
+++ bin/docker-completion.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-cd /usr/local/etc/bash_completion.d
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion            $HOME/.zsh/completion/docker.bash-completion
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion    $HOME/.zsh/completion/docker-machine.bash-completion
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion    $HOME/.zsh/completion/docker-compose.bash-completion
 
-cd $HOME/.zsh/completion
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion             $HOME/.zsh/completion/docker.zsh-completion
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.zsh-completion     $HOME/.zsh/completion/docker-machine.zsh-completion
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion     $HOME/.zsh/completion/docker-compose.zsh-completion
